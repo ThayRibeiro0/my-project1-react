4f48a2ef82619d64c7459c87ef071d1f66d9592c
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-08-12 18:10:02 -0700

    7

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,46 @@
+//first create the form and after import the hook
+import { useState } from 'react';
+
 function Form(){
     function userRegister(e){
         e.preventDefault()
-     //fluxo spa - e.preventDefault()
-        console.log('User registered')
+        console.log(`Registered the user ${name} with the password: ${password}`)
+        // console.log(name)
+    //  //fluxo spa - e.preventDefault()
+    //     console.log('User registered')
     }
+
+    const [name, setName] = useState() 
+    const [password, setPassword] = useState() 
+    // and with value default below
+    // const [name, setName] = useState('Matheus')
 
     return (
         <div>
             <h1>My register</h1>
             <form onSubmit={userRegister}>
                 <div>
-                    <input type="text" placeholder="Type your name" />
+                  {/* plus labels and id and name and onChange and with value default*/}
+                    <label htmlFor="name">Name: </label>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        placeholder="Type your name" 
+                    // value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                {/* plus the password */}
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="Type your password" 
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="Register"/>
