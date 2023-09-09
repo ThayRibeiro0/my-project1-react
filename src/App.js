25034383d5fb3a4073a67c9b3bc3596b8e7d544d
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-09-09 16:48:44 -0700

    7

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
-import { useState } from 'react';
-import './App.css';
-import YourName from './components/YourName';
-import Greatings from './components/Greatings';
-// import Condicional from './components/Condicional';
-// import Events from './components/Event';
-// import Form from './components/Form';
-// import OutraLista from './components/OtherList'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
-function App() {
-
-    const [name, setName] = useState()
-  
-  // const myItems = ['React', 'Vue', 'Angular']
-
-
+function App(){
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <YourName setName={setName}/>
-      <Greatings name={name}/>
-      {/* <h1>Renderizacao de Listas</h1>
-      <OutraLista items={myItems}/>
-      <OutraLista items={[]}/> */}
-      {/* <h1>Renderizacao Condicional</h1>
-      <Condicional/> */}
-      {/* <h1>Testando Eventos</h1> */}
-      {/* <Events />
-      <Events/> */}
-      {/* <Events number="1"/> */}
-      {/* <Events number="2"/> */}
-      {/* <Form /> */}
-    </div>
-  );
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
+// import { useState } from 'react';
+// import './App.css';
+// import YourName from './components/YourName';
+// import Greatings from './components/Greatings';
+// // import Condicional from './components/Condicional';
+// // import Events from './components/Event';
+// // import Form from './components/Form';
+// // import OutraLista from './components/OtherList'
+
+// function App() {
+
+//     const [name, setName] = useState()
+  
+//   // const myItems = ['React', 'Vue', 'Angular']
+
+
+//   return (
+//     <div className="App">
+//       <h1>State Lift</h1>
+//       <YourName setName={setName}/>
+//       <Greatings name={name}/>
+//       {/* <h1>Renderizacao de Listas</h1>
+//       <OutraLista items={myItems}/>
+//       <OutraLista items={[]}/> */}
+//       {/* <h1>Renderizacao Condicional</h1>
+//       <Condicional/> */}
+//       {/* <h1>Testando Eventos</h1> */}
+//       {/* <Events />
+//       <Events/> */}
+//       {/* <Events number="1"/> */}
+//       {/* <Events number="2"/> */}
+//       {/* <Form /> */}
+//     </div>
+//   );
+// }
+
+// export default App;
