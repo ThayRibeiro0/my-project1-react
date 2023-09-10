2b0e48e30b46719960a49520d0954a12027d3fb9
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-09-09 17:26:00 -0700

    20

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Company from './pages/Company'
 import Contact from './pages/Contact'
+import NavBar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App(){
   return (
     <Router>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/company">Company</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+      <NavBar/>
       {/* it was necessary update the routing configuration in the App.js or another relevant file. Replace the Switch component that Matheys Battisti talked about with the Routes component, like I made below */}
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route path="/company" element={<Company/>}/>
         <Route path="/contact" element={<Contact/>}/>
       </Routes>
+      <Footer/>
     </Router>
   )
 }
